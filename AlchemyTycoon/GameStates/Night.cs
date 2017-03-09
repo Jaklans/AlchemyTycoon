f50e84f3648e1b13e1ffedcb5a68e1dbdf451b6b
sxc4788 <sxc4788@GDD-37>
2017-03-09 18:10:28 -0500

    GameState

---
+++ AlchemyTycoon/GameStates/Night.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace AlchemyTycoon
+{
+    public class Night
+    {
+
+        Texture2D Background { get; set; }
+
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
