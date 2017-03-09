d4f22fb51ee014db9f5a6a0435b7c42fc2f437c5
sxc4788 <sxc4788@GDD-37>
2017-03-09 18:40:32 -0500

    Setting up the Parent Night class

--- AlchemyTycoon/GameStates/Night.cs
+++ AlchemyTycoon/GameStates/Night.cs
@@ -11,15 +11,52 @@ namespace AlchemyTycoon
 {
     public class Night
     {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D img { get; set; }
+        Rectangle imgRec { get; set; }
 
-        Texture2D Background { get; set; }
+        string imgFileName { get; set; }
+        KeyboardState kbs { get; set; }
+        //public Night()
+        //{
+        //    graphics = new GraphicsDeviceManager(this);
+        //    Cont
+        //}
+        public Night(Texture2D image, Rectangle imageRec)
+        {
+            img = image;
+            imgRec = imageRec;
+        }
+        public Night(string imageFileName)
+        {
+            imgFileName = imageFileName;
+        }
+        public Night(KeyboardState kbState)
+        {
+            kbState = kbs;
+        }
 
-
-        protected override void Draw(GameTime gameTime)
+        //protected void LoadContent()
+        //{
+        //    spriteBatch = new SpriteBatch(GraphicsDevice);
+        //
+        //    // Load the single spritesheet
+        //    spriteSheet = Content.Load<Texture2D>(imgFileName);
+        //}
+        //
+        protected void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(img, imgRec, Color.White);
+            spriteBatch.DrawString(SpriteFont,"",0,Color)
+
+            spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
