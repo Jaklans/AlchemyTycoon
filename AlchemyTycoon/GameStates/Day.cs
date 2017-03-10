086d6bd09fdc3bfca072d278d2a8e9c1adf9b9b0
MAIN\ajr8498 <ajr8498@g.rit.edu>
2017-03-09 19:14:23 -0500

    Day parent

--- AlchemyTycoon/GameStates/Day.cs
+++ AlchemyTycoon/GameStates/Day.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
 
 namespace AlchemyTycoon.GameStates
 {
     class Day
     {
+        Texture2D image;
+        Rectangle imageRec;
+
+        KeyboardState kbState;
+
+        public Day(Texture2D img, Rectangle imgr)
+        {
+            image = img;
+            imageRec = imgr;
+        }
+
+        protected void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(image, imageRec, Color.White);
+        }
+
+        protected void LoadContent(ContentManager content)
+        {
+
+        }
+
+        protected void Update()
+        {
+
+        }
     }
 }
