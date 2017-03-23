45196c01cbb3ee8b297ab5ec06be6d1b25cd8d48
mzt5441 <mzt5441@GDD-50>
2017-03-23 18:20:09 -0400

    Add

--- AlchemyTycoon/GameStates/Night.cs
+++ AlchemyTycoon/GameStates/Night.cs
@@ -13,10 +13,14 @@ namespace AlchemyTycoon
     {
         GraphicsDeviceManager graphics;
         //these will be used to get the variables we need
-        Texture2D img { get; set; }
-        Rectangle imgRec { get; set; }
-
-        SpriteBatch spriteBatch;
+        Texture2D defaultScreen;
+        Rectangle dsRec;
+        Texture2D inventoryScreen;
+        Rectangle invRec;
+        Texture2D kitScreen;
+        Rectangle ksRec;
+        Texture2D recipeScreen;
+        Rectangle rsRec;
 
         //Enum for Night's states
         enum nightState
@@ -37,48 +41,29 @@ namespace AlchemyTycoon
         //Buttons???
 
         //get all data for in game text
-        string imgFileName { get; set; }
-        SpriteFont nightFont { get; set; }
-        string nightText { get; set; }
-        Vector2 nighTextLocation { get; set; }
-        Color nightTextColor { get; set; }
-        //KeyboardState kbs { get; set; }
-        //public Night()
-        //{
-        //    graphics = new GraphicsDeviceManager(this);
-        //    Cont
-        //}
+        string imgFileName;
+        SpriteFont nightFont;
+        string nightText;
+        Vector2 nighTextLocation;
+        Color nightTextColor;
 
         //then use constructors to take the data from child into this class so that we can use them
-        public Night(Texture2D image, Rectangle imageRec)
-        {
-            img = image;
-            imgRec = imageRec;
-        }
-        public Night(string imageFileName)
-        {
-            imgFileName = imageFileName;
-        }
-        public Night(SpriteFont font, string text, Vector2 textlocation, Color tcolor)
-        {
-            nightFont = font;
-            nightText = text;
-            nighTextLocation = textlocation;
-            nightTextColor = tcolor;
-        }
-        //public Night(KeyboardState kbState)
+        //public Night(Texture2D image, Rectangle imageRec)
         //{
-        //    kbState = kbs;
+        //    img = image;
+        //    imgRec = imageRec;
         //}
-        //
-        //protected void LoadContent()
+        //public Night(string imageFileName)
         //{
-        //    spriteBatch = new SpriteBatch(GraphicsDevice);
-        //
-        //    // Load the single spritesheet
-        //    spriteSheet = Content.Load<Texture2D>(imgFileName);
+        //    imgFileName = imageFileName;
         //}
-        //
+        //public Night(SpriteFont font, string text, Vector2 textlocation, Color tcolor)
+        //{
+        //    nightFont = font;
+        //    nightText = text;
+        //    nighTextLocation = textlocation;
+        //    nightTextColor = tcolor;
+        //}
 
         //LoadContent method
         protected void LoadContent()
@@ -104,12 +89,12 @@ namespace AlchemyTycoon
         {
             // TODO: Add your drawing code here
             
-            spriteBatch.Draw(img, imgRec, Color.White);
-            spriteBatch.DrawString(nightFont, nightText, nighTextLocation, nightTextColor);
+            //spriteBatch.Draw(img, imgRec, Color.White);
+            //spriteBatch.DrawString(nightFont, nightText, nighTextLocation, nightTextColor);
 
             switch(currentState)
             {
-                case nightState.Default
+                case nightState.Default:
                     spriteBatch.Draw()
                     break;
                 case nightState.Inventory:
